3a8e5f758db19b88df5097375c68f3b428fd7e53
AriyanMlzm <ryanmolazem@gmail.com>
2025-03-02 15:13:27 +0330

    add costume error comp

---
+++ src/routes/+error.svelte
@@ -0,0 +1,8 @@
+<section class="w-full h-full flex-col flex-center bg-primary">
+	<img draggable="false" src="/404.png" alt="not-found" width="500px" />
+	<button
+		class="bg-white cursor-pointer text-[20px] rounded-[20px] py-[10px] px-[20px] font-bold hover:bg-primary hover:text-white active:bg-primary active:text-white text-primary"
+		on:click={() => window.location.href = '/'}
+		type="button">Home</button
+	>
+</section>
